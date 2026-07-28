b5af8cb65f9918db525dc78b415968b6226c4a12
PirateKenny01 <shadab.fardin0234@gmail.com>
2026-07-28 23:56:42 +0600

    update-profile slightly changed

--- src/members/dto/update-profile.dto.ts
+++ src/members/dto/update-profile.dto.ts
@@ -1,25 +1,29 @@
-import { IsOptional, IsString, IsEmail, IsEnum, Matches } from 'class-validator';
+import { IsOptional, IsString, IsEmail, IsEnum, Matches, IsEmpty, IsNotEmpty, IsDateString } from 'class-validator';
 
-export class UpdateProfileDto {
-  // Lab Task 2 — Pipes: Category 1 rule — name should contain only alphabets
+export class UpdateProfileDto 
+{
+  //name should contain only alphabets
   @IsOptional()
   @IsString()
-  @Matches(/^[A-Za-z\s]+$/, {
+  @Matches(/^[A-Za-z\s]+$/, 
+  {
     message: 'Name should contain only alphabets',
   })
   name?: string;
 
-  // Lab Task 2 — Pipes: Category 1 rule — email must be required and contain @ and a .xyz domain
-  @IsOptional()
+  //email must be required and contain @ and a .xyz domain
+  @IsNotEmpty({ message: 'Email Address is required' })
   @IsEmail({}, { message: 'Invalid email format' })
-  @Matches(/^[^\s@]+@[^\s@]+\.xyz$/, {
-    message: 'Email must be a valid .xyz domain address',
+  @Matches(/^[^\s@]+@[^\s@]+\.xyz$/, 
+  {
+     message: 'Email must be a valid .xyz domain address',
   })
-  email?: string;
-
-  // Lab Task 2 — Pipes: Category 1 rule — validate NID number format (10, 13, or 17 digit NID)
+  email!: string; // Note: removed the '?' because it is no longer optional
+  
+  //validate NID number format (10, 13, or 17 digit NID)
   @IsOptional()
-  @Matches(/^(\d{10}|\d{13}|\d{17})$/, {
+  @Matches(/^(\d{10}|\d{13}|\d{17})$/,
+  {
     message: 'NID must be a valid 10, 13, or 17 digit number',
   })
   nid?: string;
