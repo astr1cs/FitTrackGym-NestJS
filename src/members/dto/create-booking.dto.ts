560e455bed6eabd520211273c8b94a7d574e34c6
astr1cs <merazuddin003@gmail.com>
2026-07-28 21:29:57 +0600

    branch cleanup

--- src/members/dto/create-booking.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class CreateBookingDto {
-  @IsNotEmpty({ message: 'Class ID is required' })
-  @IsString()
-  class_id: string;
-}
--- src/members/dto/membership-subscription.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class MembershipSubscriptionDto {
-  @IsNotEmpty({ message: 'Plan ID is required' })
-  @IsString()
-  plan_id: string;
-}
