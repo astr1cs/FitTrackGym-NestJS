60a72271d5ca7b16e0af790fea11bf88387506f0
PirateKenny01 <shadab.fardin0234@gmail.com>
2026-07-28 23:49:26 +0600

    clean routes for member user

--- src/trainer/trainer.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TrainerController } from './trainer.controller';
-import { TrainerService } from './trainer.service';
-
-@Module({
-  controllers: [TrainerController],
-  providers: [TrainerService],
-})
-export class TrainerModule {}
